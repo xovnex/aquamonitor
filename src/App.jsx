6cf99f9adea8c89ca3553a4ec8bf5986c1ec2d5b
xovnex <gianfrancodelacruzlopez123@gmail.com>
2026-04-02 13:32:15 -0500

    4
    4

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AppLayout from "./components/layout/AppLayout";
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/registro";
+import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
 import HistorialPage from "./pages/HistorialPage";
 import ConfiguracionPage from "./pages/ConfiguracionPage";
